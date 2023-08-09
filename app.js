5eb4a659ba3038f5920f7b1b7f099cfe66e01f24
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2023-08-09 11:59:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  const { status = 500, message = error.message } = error;
+  const { status = 500, message = "Server error" } = error;
   res.status(status).json({ message });
 });
 
