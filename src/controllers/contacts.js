d3f606ed45f34da67720f4aeda6604e4eecb92bb
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2023-08-09 11:38:52 +0300

    corrected

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,12 @@
 const { ApiError } = require("../helpers/apiError");
-const { Schema, model, patchFavouriteSchema } = require("mongoose");
+const {
+  Schema,
+  model,
+  patchFavouriteSchema,
+  postSchema,
+  putSchema,
+} = require("mongoose");
+const ctrlWrapper = require("../helpers/ctrlWrapper");
 const contactSchema = new Schema({
   name: {
     type: String,
@@ -37,33 +44,47 @@ const removeContactById = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndRemove(id);
   if (!result) {
-    throw ApiError(404);
+    throw ApiError(404, error.message);
   }
   res.json({ message: "Deleted successfully" });
 };
+
 const createContact = async (req, res) => {
-  const result = await Contact.create(req.body);
-  res.status(201).json(result);
+  const { error } = postSchema.validate(req.body);
+  if (error) {
+    res.json("All the fields are required");
+  } else {
+    const result = await Contact.create(req.body);
+    res.status(201).json(result);
+  }
 };
+
 const updateContact = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
-  if (!result) {
-    throw ApiError(404, "Not Found");
+  const { error } = putSchema.validate(req.body);
+  if (error) {
+    throw ApiError(400, error.message);
+  } else {
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    if (result === null || !result) {
+      throw ApiError(404, error.message);
+    }
+
+    res.status(201).json(result);
   }
-  res.status(201).json(result);
 };
+
 const checkFavourite = async (req, res) => {
   const { error } = patchFavouriteSchema.validate(req.body);
   if (error) {
-    res.status(400).json("Missing field favorite");
+    throw ApiError(400, "Missing field favorite");
   } else {
     const result = await Contact.findByIdAndUpdate(
       req.params.contactId,
       req.body
     );
     if (result === null) {
-      res.status(404).json(result);
+      throw ApiError(404, error.message);
     } else {
       switch (result.favourite) {
         case true:
@@ -78,12 +99,13 @@ const checkFavourite = async (req, res) => {
     }
   }
 };
+
 module.exports = {
   Contact,
-  getContacts,
-  getContactById,
-  removeContactById,
-  createContact,
-  updateContact,
-  checkFavourite,
+  getContacts: ctrlWrapper(getContacts),
+  getContactById: ctrlWrapper(getContactById),
+  removeContactById: ctrlWrapper(removeContactById),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+  checkFavourite: ctrlWrapper(checkFavourite),
 };
--- src/routes/api/contacts.js
+++ src/routes/api/contacts.js
@@ -8,5 +8,5 @@ router.get("/:contactId", controllers.getContactById);
 router.delete("/:contactId", controllers.removeContactById);
 router.post("/", controllers.createContact);
 router.put("/:contactId", controllers.updateContact);
-router.patch("/api/contacts/:contactId/favorite", controllers.checkFavourite);
+router.patch("/:contactId/favorite", controllers.checkFavourite);
 module.exports = router;
